b29d891fedafe0374ebda063f04498717d5d799a
JQ H <TknHJQ@outlook.com>
2020-12-19 23:46:39 +0800

    2020.12.19

--- MatrixGraph.cpp
+++ MatrixGraph.cpp
@@ -2,3 +2,46 @@
 #include "MatrixGraph.h"
 using namespace std;
 
+//构造函数
+template<class DataType>
+inline MatrixGraph<DataType>::MatrixGraph(DataType input[], int vertex, int edge)
+{
+	vertexNum = vertex; edgeNum = edge;
+	//存储图的顶点
+	for (int index = 0，index < vertexNum; index++)
+		vertex[index] = input[index];
+	//将邻接矩阵置零
+	for (int index = 0; index < vertexNum; index++)
+	{
+		for (int index1 = 0; index1 < vertexNum; index1++)
+		{
+			edge[index][index1] = 0;
+		}
+	}
+	//存储图的边信息
+	for (int index = 0; k < edgeNum; index++)
+	{
+
+	}
+}
+
+//析构函数
+template<class DataType>
+inline MatrixGraph<DataType>::~MatrixGraph()
+{
+
+}
+
+//深度优先遍历
+template<class DataType>
+inline void MatrixGraph<DataType>::DeepFirstTraverse(int v)
+{
+
+}
+
+//广度优先遍历
+template<class DataType>
+inline void MatrixGraph<DataType>::BreathFirstTraverse(int v)
+{
+
+}
--- MatrixGraph.h
+++ MatrixGraph.h
@@ -10,7 +10,8 @@ private:
 	bool edge[MAXSIZE][MAXSIZE];//邻接矩阵
 	int vertexNum, edgeNum;//顶点数和边数
 public:
-	MatrixGraph(DataType data[], int vertex, int edge);//构造函数
+	MatrixGraph(DataType input[], int vertex, int edge);//构造函数
 	~MatrixGraph();//析构函数
-	void DeepFirstTraverse(int )
+	void DeepFirstTraverse(int v);//深度优先遍历
+	void BreathFirstTraverse(int v);//广度优先遍历
 };
