bf73666a5d8acea959bd8de834fe960bea26cf7c
JQ H <TknHJQ@outlook.com>
2020-12-20 21:20:16 +0800

    2020.12.20

--- MatrixGraph.cpp
+++ MatrixGraph.cpp
@@ -2,11 +2,34 @@
 #include "MatrixGraph.h"
 using namespace std;
 
+//深度优先遍历
+template<class DataType>
+void MatrixGraph<DataType>::DFT(int v, bool visited[])
+{
+	cout << "已访问：" << vertex[v];
+	visited[v] = true;
+	for (int index = 0; index < vertexNum; index++)
+		if (edge[v][index] == 1 && visited[index] == 0)
+			DFT(index, visited);
+}
+
+//深度优先遍历
+template<class DataType>
+void MatrixGraph<DataType>::BFT(int v, bool visited[])
+{
+	int Queue[MAXSIZE];//队列
+	int front, rear = -1;//初始化队列
+	cout << "已访问：" << vertex[v];
+	visited[v] = true;
+
+}
+
 //构造函数
 template<class DataType>
 inline MatrixGraph<DataType>::MatrixGraph(DataType input[], int vertex, int edge)
 {
-	vertexNum = vertex; edgeNum = edge;
+	vertexNum = vertex;
+	edgeNum = edge;
 	//存储图的顶点
 	for (int index = 0，index < vertexNum; index++)
 		vertex[index] = input[index];
@@ -19,29 +42,26 @@ inline MatrixGraph<DataType>::MatrixGraph(DataType input[], int vertex, int edge
 		}
 	}
 	//存储图的边信息
-	for (int index = 0; k < edgeNum; index++)
+	for (int index = 0, int index1, int index2 ; k < edgeNum; index++)
 	{
-
+		cout << "请输入边的两个顶点的序号：";
+		cin >> index1 >> index2;
+		edge[index1][index2];
 	}
-}
-
-//析构函数
-template<class DataType>
-inline MatrixGraph<DataType>::~MatrixGraph()
-{
-
 }
 
 //深度优先遍历
 template<class DataType>
 inline void MatrixGraph<DataType>::DeepFirstTraverse(int v)
 {
-
+	bool visited[MAXSIZE] = false;
+	DFT(int v, bool visited[]);
 }
 
 //广度优先遍历
 template<class DataType>
 inline void MatrixGraph<DataType>::BreathFirstTraverse(int v)
 {
-
+	bool visited[MAXSIZE] = false;
+	BFT(int v, bool visited[]);
 }
--- MatrixGraph.h
+++ MatrixGraph.h
@@ -1,6 +1,6 @@
 #pragma once
 
-const int MAXSIZE;
+const int MAXSIZE = 16;
 
 template<class DataType>
 class MatrixGraph
@@ -9,9 +9,10 @@ private:
 	DataType vertex[MAXSIZE];//顶点数组
 	bool edge[MAXSIZE][MAXSIZE];//邻接矩阵
 	int vertexNum, edgeNum;//顶点数和边数
+	void DFT(int v, bool visited[]);//深度优先遍历
+	void BFT(int v, bool visited[]);//广度优先遍历
 public:
 	MatrixGraph(DataType input[], int vertex, int edge);//构造函数
-	~MatrixGraph();//析构函数
 	void DeepFirstTraverse(int v);//深度优先遍历
 	void BreathFirstTraverse(int v);//广度优先遍历
 };
