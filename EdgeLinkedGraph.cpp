b45542e8c4c90c282d7dce4a063db6e35d56bcc1
JQ H <TknHJQ@outlook.com>
2020-12-22 16:11:42 +0800

    2020.12.22

--- EdgeLinkedGraph.cpp
+++ EdgeLinkedGraph.cpp
@@ -1,4 +1,57 @@
 #include <iostream>
-#include "MatrixGraph.h"
+#include "EdgeLinkedGraph.h"
 using namespace std;
 
+//构造函数
+template<class DataType>
+inline EdgeLinkedGraph<DataType>::EdgeLinkedGraph(DataType data[], int vertexNum, int edgeNum)
+{
+	this->vertexNum = vertexNum;
+	this->edgeNum = edgeNum;
+	for (int index = 0; index < VertexNum; index++)
+	{
+		adjlist[index]->vertex = data[index];
+		adjlist[index]->firstedge = nullptr;
+	}
+	for (int index = 0, index1, index2, EdgeNode* temp; k < edgeNum; index++)
+	{
+		cout << "请输入边两个顶点的序号：";
+		cin >> index1 >> index2;
+		temp = new EdgeNode;
+		temp.next = adjlist[index1].firstedge;
+		adjlist[i].firstedge = temp;
+	}
+}
+
+//析构函数
+template<class DataType>
+EdgeLinkedGraph<DataType>::~EdgeLinkedGraph()
+{
+	EdgeNode* ptr = nullptr;
+	for (int index = 0; index < vertexNum; index++)
+	{
+		ptr = adjlist[index]->firstedge;
+		while (ptr!+ nullptr)
+		{
+			adjlist[index]->firstedge = ptr->next;
+			delete ptr;
+		}
+	}
+}
+
+//深度优先遍历
+template<class DataType>
+inline void EdgeLinkedGraph<DataType>::DeepFirstTraverse(int v)
+{
+	EdgeNode* ptr = nullptr;
+	int index;
+	cout << "已访问：" << adjlist[v].vertex << endl;
+
+}
+
+//广度优先遍历
+template<class DataType>
+inline void EdgeLinkedGraph<DataType>::BreathFirstTraverse(int v)
+{
+
+}
--- EdgeLinkedGraph.h
+++ EdgeLinkedGraph.h
@@ -1,6 +1,29 @@
 #pragma once
 
+//边结点
 struct EdgeNode
 {
-	
+	int adjvex;
+	EdgeNode* next;
+};
+
+//点结点
+template<class DataType>
+struct VertexNode
+{
+	DataType vertex;
+	EdgeNode* firstedge;
+};
+
+template<class DataType>
+class EdgeLinkedGraph
+{
+private:
+	VertexNode<DataType> adjlist[MAXSIZE];//存放顶点表的数组
+	int vertexNum, edgeNum;//顶点数和边数
+public:
+	EdgeLinkedGraph(DataType data[], int vertexNum, int edgeNum);//构造函数
+	~EdgeLinkedGraph();//析构函数
+	void DeepFirstTraverse(int v);//深度优先遍历
+	void BreathFirstTraverse(int v);//广度优先遍历
 };
